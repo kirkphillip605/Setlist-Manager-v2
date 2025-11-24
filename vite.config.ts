e71480788e0516b4cef9062d3a7cd904b9063a01
Phillip Kirk <phillipkirk7@gmail.com>
2025-11-24 10:55:36 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig(({ mode }) => {
   
   const port = parseInt(env.PORT || '8080');
   const host = env.HOST || '::';
+  const allowedHosts = (env.ALLOWED_HOSTS || "")
+    .split(",")
+    .map(host => host.trim())
+    .filter(Boolean);
+
+  console.log(allowedHosts);
 
   return {
     server: {
@@ -17,13 +23,24 @@ export default defineConfig(({ mode }) => {
       port: port,
       // Development server CORS
       cors: true,
+      allowedHosts: allowedHosts,      
     },
     preview: {
       host: host,
       port: port,
       // Preview server CORS
       cors: true,
+      allowedHosts: allowedHosts,       
     },
+    dev: {
+      host: host,
+      port: port,
+      // Preview server CORS
+      cors: true,
+      allowedHosts: allowedHosts,     
+    },
+
+
     plugins: [dyadComponentTagger(), react()],
     resolve: {
       alias: {
