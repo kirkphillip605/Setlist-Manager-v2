a6c74952c6b64cc3e8a02f410bd16a7a63f46e4e
[dyad] <phillipkirk7@gmail.com>
2025-12-21 18:58:10 -0600

    Updated android/app/src/main/java/com/kirknetllc/setlistpro/MainActivity.java

--- android/app/src/main/java/com/kirknetllc/setlistpro/MainActivity.java
+++ android/app/src/main/java/com/kirknetllc/setlistpro/MainActivity.java
@@ -1,5 +1,42 @@
 package com.kirknetllc.setlistpro;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        applyFullscreen();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyFullscreen();
+        }
+    }
+
+    private void applyFullscreen() {
+        Window window = getWindow();
+        View decorView = window.getDecorView();
+
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, decorView);
+        if (controller == null) return;
+
+        controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+
+        controller.hide(WindowInsetsCompat.Type.statusBars() | WindowInsetsCompat.Type.navigationBars());
+    }
+}
